255c23d9465ace0a2b69530c218743b1313605e0
mariasfiraiala <maria.sfiraiala@gmail.com>
2022-07-01 16:27:54 +0300

    Add dump function
    Signed-off-by: mariasfiraiala <maria.sfiraiala@gmail.com>

--- alloc_utils.c
+++ alloc_utils.c
@@ -19,3 +19,22 @@ void finalize(arena_t *arena)
     free(arena->byte_string);
     free(arena);
 }
+
+void dump(arena_t *arena)
+{
+    for (int i = 0; i < arena->size; i += 16) {
+        printf("%08X\t", i);
+        for (int j = 0; j < 16 && i + j < arena->size; ++j) {
+            char ch = *((char *)(arena->byte_string + i + j));
+
+            if (j == 7)
+                printf("%02X  ", ch);
+            else
+                printf("%02X ", ch);
+        }
+
+        printf("\n");
+    }
+
+    printf("%08X\n", arena->size);
+}
